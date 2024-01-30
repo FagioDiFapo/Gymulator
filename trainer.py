386927221f717bfca7bc735fa404d2d022ee8f1a
FagioDiFapo <genboga.bbv@gmail.com>
2024-01-30 19:40:21 +0100

    Trainer fixes

--- trainer.py
+++ trainer.py
@@ -1,3 +1,4 @@
+import os
 import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
@@ -15,16 +16,14 @@ def get_arguments():
 
 class Trainer:
 
-    MODEL = None
-
     def __init__(self, model_name):
         self.model_name = model_name
-
-    def train(self, timesteps, nenvs):
-        envs = make_vec_env(RocketLander, n_envs=nenvs)
-        self.MODEL = PPO(
+        if os.path.isfile(self.model_name+".zip"):
+            self.model = PPO.load(self.model_name+".zip")
+            self.model_name += "v2"
+        else:
+            self.model = PPO(
             policy = 'MlpPolicy',
-            env = envs,
             n_steps = 1024,
             batch_size = 64,
             n_epochs = 4,
@@ -33,14 +32,19 @@ class Trainer:
             ent_coef = 0.05,
             verbose=1,
             device="cuda")
-        self.MODEL.learn(total_timesteps=timesteps)
-        self.MODEL.save(self.model_name)
+
+    def train(self, timesteps, nenvs):
+        envs = make_vec_env(RocketLander, n_envs=nenvs)
+        self.model.set_env(envs)
+
+        self.model.learn(total_timesteps=timesteps)
+        self.model.save(self.model_name)
 
     def test(self):
         eval_env = Monitor(RocketLander(render_mode = "human"))
-        if self.MODEL == None: self.MODEL = PPO.load(self.model_name+".zip")
+        self.model.set_env(eval_env)
 
-        mean_reward, std_reward = evaluate_policy(self.MODEL, eval_env, n_eval_episodes=10, deterministic=True)
+        mean_reward, std_reward = evaluate_policy(self.model, eval_env, n_eval_episodes=10, deterministic=True)
         print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
 
 
